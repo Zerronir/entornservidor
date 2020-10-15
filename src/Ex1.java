aa974ee62b39607251905e27fad83b4402234010
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-10-15 17:25:17 +0200

    Diferencias entre programación primitiva y funcional

--- src/Ex1.java
+++ src/Ex1.java
@@ -7,30 +7,41 @@ public class Ex1 {
         * Programación funcional
         * */
         Integer[] ar = new Integer[]{1,2,3,4,5,6,7};
-        Print print = new Print();
 
         /*
         * Maneras de imprimir primitivas
-        * */
-
-        //System.out.println(Arrays.toString(ar));
-
-        /*for (int i = 0; i < ar.length; i++) {
+        *
+        * System.out.println(Arrays.toString(ar));
+        *
+        * for (int i = 0; i < ar.length; i++) {
             System.out.println(ar[i]);
-        }*/
+          }
+        *
+        * */
 
         /*
         * Programacion funcional
         *
         * Stream -> Tipo de array
         * stream().count() -> devuelve un long
+        *
+        * //count() devuelve un long con el número de elementos de la array
+        * long numEl = Arrays.stream(ar).count();
+        *
         * */
 
-        //count() devuelve un long con el número de elementos de la array
-        long numEl = Arrays.stream(ar).count();
+        /*
+        * forEach() -> necesita de un objeto para ejecutar sus métodos
+        * MANERAS DE HACER EL FOREACH
+        *
+        * 1. Arrays.stream(ar).forEach(print); Que necesita el siguiente objeto -> Print print = new Print();
+        * 2. Arrays.stream(ar).forEach((i) -> System.out.println(i));
+        * 3. Consumer<Integer> print = (i) -> System.out.println(i);
+        *
+        * */
 
-        //forEach() -> necesita de un objeto para ejecutar sus métodos
-        Arrays.stream(ar).forEach(print);
+        Arrays.stream(ar).forEach((i) -> System.out.println(i));
+
 
     }
 
