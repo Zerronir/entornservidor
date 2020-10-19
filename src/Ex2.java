2d7037d0509a2d8a34082d02e4c545bcfc6668b1
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-10-19 16:20:11 +0200

    19-10-2020

---
+++ src/Ex2.java
@@ -0,0 +1,27 @@
+import java.security.cert.CertificateParsingException;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Singleton s1 = new Singleton();
+        Singleton s2 = new Singleton();
+
+        s1.setValor(5);
+        s2.setValor(7);
+
+        System.out.println(s1.getValor() + " " + s2.getValor());
+
+    }
+}
+
+class Singleton {
+
+    private int valor;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}
